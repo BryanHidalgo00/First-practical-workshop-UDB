27403646ac5bda0354c36c2e666c0bdbeb5e7423
Bryan Hidalgo <88019841+BryanHidalgo00@users.noreply.github.com>
2022-09-07 10:30:47 -0600

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,21 @@
+//div effect display 
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".body").fadeOut(80);
+    }, 80);
+
+    setTimeout(function () {
+        $(".body").fadeIn(1500);
+    }, 500);
+});
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".crud").fadeOut(80);
+    }, 80);
+
+    setTimeout(function () {
+        $(".crud").fadeIn(1500);
+    }, 2000);
+});
+
