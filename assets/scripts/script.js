ea8ff5b989afc356814ae8a9c4878b1c66135100
BryanHidalgo00 <bryanmj2021@gmail.com>
2022-09-08 11:42:37 -0600

    1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,33 +1,194 @@
 let crudForm = document.querySelector(".crud");
-let btnToggleForm = document.querySelector(".floatIcon")
-let worldEvents = {
-    
-};
-btnToggleForm.addEventListener("click", () => {
-    if (crudForm.style.display === "flex") {
-        crudForm.style.display = "none";
-        btnToggleForm.querySelector("i").classList.remove("fa-solid", "fa-xmark");
-        btnToggleForm.querySelector("i").classList.add("fa-sharp", "fa-solid", "fa-check");
+let btnToggleForm = document.querySelector(".floatIcon");
+let clickSubmit = document.querySelector("#btnSend");
+let worldEvents = [];
 
-    } else {
-        btnToggleForm.querySelector("i").classList.remove("fa-sharp", "fa-solid", "fa-check");
-        btnToggleForm.querySelector("i").classList.add("fa-solid", "fa-xmark");
-        crudForm.style.display = "flex";
-    }
-})
+let btnDeletes = "";
 
+document.addEventListener("DOMContentLoaded", () => {
+  worldEvents = JSON.parse(localStorage.getItem("worldEvents")) || [];
+  let output = "";
 
-document.getElementById("btnSend").addEventListener("click", function (event) {
-    event.preventDefault()
+  worldEvents.forEach((event, index) => {
+    let { title, year, yearEnd, message, id } = event;
+    let rowIndex = index + 1;
+
+    output += `
+    <div class="row row-${rowIndex % 2 === 0 ? "2" : "1"}">
+            <section>
+                <i class="icon fa-solid fa-heart"></i>
+                <div class="details">
+                    <span class="title">${title}</span>
+                    <span>${year}</span>
+                </div>
+                <p>${message}</p>
+                <div class="bottom">
+                    <div class="bottom-c">
+                        <btn class="btn-a1" href="#">Modify event</btn>
+                        <btn class="btn-a2 btnDelete" value="${id}">Delete event</btn>
+                    </div>
+                    <i>- ${yearEnd}</i>
+                </div>
+            </section>
+        </div>
+    `;
+  });
+
+  rows.innerHTML = output;
+  btnDeletes = document.querySelectorAll(".btnDelete");
+
+  btnDeletes.forEach((btnDelete) => {
+    btnDelete.addEventListener("click", () => {
+      console.log("CLICk");
+
+      let id = btnDelete.getAttribute("value");
+      let filteredEvents = worldEvents.filter((event) => event.id != id);
+      worldEvents = filteredEvents;
+      let output = "";
+
+      worldEvents.forEach((event, index) => {
+        let { title, year, yearEnd, message, id } = event;
+        let rowIndex = index + 1;
+
+        output += `
+            <div class="row row-${rowIndex % 2 === 0 ? "2" : "1"}">
+                    <section>
+                        <i class="icon fa-solid fa-heart"></i>
+                        <div class="details">
+                            <span class="title">${title}</span>
+                            <span>${year}</span>
+                        </div>
+                        <p>${message}</p>
+                        <div class="bottom">
+                            <div class="bottom-c">
+                                <btn class="btn-a1" href="#">Modify event</btn>
+                                <btn class="btn-a2 btnDelete" value="${id}">Delete event</btn>
+                            </div>
+                            <i>- ${yearEnd}</i>
+                        </div>
+                    </section>
+                </div>
+            `;
+      });
+
+      rows.innerHTML = output;
+
+      localStorage.setItem("worldEvents", JSON.stringify(worldEvents));
+      setTimeout(() => {
+        btnDeletes = document.querySelectorAll(".btnDelete");
+      }, 1000);
+    });
+  });
 });
 
-// //div effect display 
-// $(document).ready(function () {
-//     setTimeout(function () {
-//         $(".body").fadeOut(0);
-//     }, 80);
+let rows = document.querySelector(".rows");
 
-//     setTimeout(function () {
-//         $(".body").fadeIn(1500);
-//     }, 500);
-// });
+btnToggleForm.addEventListener("click", () => {
+  if (crudForm.style.display === "flex") {
+    crudForm.style.display = "none";
+    btnToggleForm.querySelector("i").classList.remove("fa-solid", "fa-xmark");
+    btnToggleForm
+      .querySelector("i")
+      .classList.add("fa-sharp", "fa-solid", "fa-check");
+  } else {
+    btnToggleForm
+      .querySelector("i")
+      .classList.remove("fa-sharp", "fa-solid", "fa-check");
+    btnToggleForm.querySelector("i").classList.add("fa-solid", "fa-xmark");
+    crudForm.style.display = "flex";
+  }
+});
+
+clickSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+  let title = document.querySelector("#title").value;
+  let year = document.querySelector("#year").value;
+  let yearEnd = document.querySelector("#yearEnd").value;
+  let message = document.querySelector("#message").value;
+  let id = new Date().getTime();
+  let worldEvent = {
+    title,
+    year,
+    yearEnd,
+    message,
+    id,
+  };
+  worldEvents.push(worldEvent);
+
+  let output = "";
+
+  worldEvents.forEach((event, index) => {
+
+    let { title, year, yearEnd, message, id } = event;
+    let rowIndex = index + 1;
+
+    output += `
+    <div class="row row-${rowIndex % 2 === 0 ? "2" : "1"}">
+            <section>
+                <i class="icon fa-solid fa-heart"></i>
+                <div class="details">
+                    <span class="title">${title}</span>
+                    <span>${year}</span>
+                </div>
+                <p>${message}</p>
+                <div class="bottom">
+                    <div class="bottom-c">
+                        <btn class="btn-a1" href="#">Modify event</btn>
+                        <btn class="btn-a2 btnDelete" value="${id}">Delete event</btn>
+                    </div>
+                    <i>- ${yearEnd}</i>
+                </div>
+            </section>
+        </div>
+    `;
+  });
+
+  rows.innerHTML = output;
+
+  localStorage.setItem("worldEvents", JSON.stringify(worldEvents));
+  btnDeletes = document.querySelectorAll(".btnDelete");
+
+  btnDeletes.forEach((btnDelete) => {
+    btnDelete.addEventListener("click", () => {
+      console.log("CLICk");
+
+      let id = btnDelete.getAttribute("value");
+      let filteredEvents = worldEvents.filter((event) => event.id != id);
+      worldEvents = filteredEvents;
+      localStorage.setItem("worldEvents", JSON.stringify(worldEvents));
+      let output = "";
+
+      worldEvents.forEach((event, index) => {
+
+        let { title, year, yearEnd, message, id } = event;
+        let rowIndex = index + 1;
+
+        output += `
+            <div class="row row-${rowIndex % 2 === 0 ? "2" : "1"}">
+                    <section>
+                        <i class="icon fa-solid fa-heart"></i>
+                        <div class="details">
+                            <span class="title">${title}</span>
+                            <span>${year}</span>
+                        </div>
+                        <p>${message}</p>
+                        <div class="bottom">
+                            <div class="bottom-c">
+                                <btn class="btn-a1" href="#">Modify event</btn>
+                                <btn class="btn-a2 btnDelete" value="${id}">Delete event</btn>
+                            </div>
+                            <i>- ${yearEnd}</i>
+                        </div>
+                    </section>
+                </div>
+            `;
+      });
+
+      rows.innerHTML = output;
+      setTimeout(() => {
+        btnDeletes = document.querySelectorAll(".btnDelete");
+      }, 1000);
+
+    });
+  });
+});
